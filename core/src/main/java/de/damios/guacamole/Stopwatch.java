1b0dd58494b75dc5dd6a2ef5d35c54b082527a4d
crykn <crykn@users.noreply.github.com>
2023-07-07 11:19:43 +0200

    Minor improvements to IntRange and Stopwatch.

--- core/src/main/java/de/damios/guacamole/Stopwatch.java
+++ core/src/main/java/de/damios/guacamole/Stopwatch.java
@@ -1,6 +1,25 @@
+/*
+ * Copyright 2023 damios
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.damios.guacamole;
+
 import java.util.concurrent.TimeUnit;
 
+/**
+ * A simple stopwatch class to measure time.
+ */
 public class Stopwatch {
 
 	public static Stopwatch createUnstarted() {
@@ -41,12 +60,12 @@ public class Stopwatch {
 		return isRunning;
 	}
 
-	private long getTime() {
+	public long getTime() {
 		return isRunning ? System.nanoTime() - startTime + elapsedTime
 				: elapsedTime;
 	}
 
-	private long getTime(TimeUnit unit) {
+	public long getTime(TimeUnit unit) {
 		return unit.convert(getTime(), TimeUnit.NANOSECONDS);
 	}
 
